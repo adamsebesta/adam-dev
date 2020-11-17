61045482d83cf7d755a4e1b51ec6d77439f22b24
Adam Sebesta <adam.sebesta5@gmail.com>
2020-11-17 11:35:56 +0100

    revise server

--- middleware/api/v1/index.js
+++ middleware/api/v1/index.js
@@ -1,12 +1,13 @@
 const httpProxy = require('http-proxy')
 let API_URL = process.env.LOGIC_APP_URL
 const proxy = httpProxy.createProxyServer({
-  target: endpoint,
+  target: API_URL,
   secure: false,
   changeOrigin: true
 })
 
 export default async function(req, res, next) {
+  console.log(API_URL);
   // await fetch(API_URL, {
   //   method: 'POST',
   //   body: JSON.stringify(req.body),
