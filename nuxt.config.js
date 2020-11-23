8a5d468bc4df91d56a8a093997fefc02e007449e
Adam Sebesta <adam.sebesta5@gmail.com>
2020-11-23 10:15:41 +0100

    refactor envs in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ module.exports = {
   ],
 
   privateRuntimeConfig: {
-    logicAppUrl: "https://prod-57.eastus.logic.azure.com:443/workflows/f50c9b5ca3a14edc88a4910085747591/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=V0WsnUByfGtKvlhwwn-RcwueeFwnxn2FWV_r8FnNqPM"
+    logicAppUrl: process.env.LOGIC_APP_URL,
   },
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
