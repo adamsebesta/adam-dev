fde3da268ee1c5c3a53c3828f043c5f04c2d5ee4
Adam Sebesta <adam.sebesta5@gmail.com>
2021-02-28 15:18:53 +0100

    working on meta index image

--- pages/index.vue
+++ pages/index.vue
@@ -106,12 +106,12 @@ export default {
         {
           hid: "og:image",
           property: "og:image",
-          content: this.BASE_URL + meta,
+          content: "https://www.asdevelopment.ca/meta.png",
         },
         {
           hid: "twitter:image",
           property: "twitter:image",
-          content: this.BASE_URL + meta,
+          content: "https://www.asdevelopment.ca/meta.png",
         },
       ],
     };
